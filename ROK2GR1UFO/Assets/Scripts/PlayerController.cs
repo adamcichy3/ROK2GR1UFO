0f72786483db01452fd77e6802a908f5d06fa8b8
Adam Cichy <49528755+adamcichyy@users.noreply.github.com>
2023-05-10 18:01:17 +0200

    Added scripts Sound & AudioManager

--- ROK2GR1UFO/Assets/Scripts/PlayerController.cs
+++ ROK2GR1UFO/Assets/Scripts/PlayerController.cs
@@ -40,15 +40,27 @@ public class PlayerController : MonoBehaviour
     {
         scoreText.text = "Wynik: " + count;
         if (count == 3)
-        {
+        { 
             winText.gameObject.SetActive(true);
             scoreText.gameObject.SetActive(false);
-            //Thread.Sleep(5000);
-            SceneManager.LoadScene("Level02");
-
+            StartCoroutine(StopTime());
         }
-
     }
+
+    IEnumerator StopTime()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Level02");
+    }
+
+
+    
+
+
+
+    
+
+
 
  
 
